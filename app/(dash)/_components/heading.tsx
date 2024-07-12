1f955066aaf4238a3d5311235f7330aad763843e
fobic <sabychakraborty08@gmail.com>
2024-07-13 05:04:41 +0530

    func(auth) : added authentication

--- app/(dash)/_components/heading.tsx
+++ app/(dash)/_components/heading.tsx
@@ -1,21 +1,45 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export const Heading = () => {
-    return (
-        <div className="max-w-3xl space-y-4">
-            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-               An Intuitive Alternative to Notion. Experience more with <span className="underline">Hitch</span> 
-            </h1>
-            <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-                Hitch is a connected workspace where <br/> ideas come to fruition.
-            </h3>
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        An Intuitive Alternative to Notion. Experience more with{" "}
+        <span className="underline">Hitch</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Hitch is a connected workspace where <br /> ideas come to fruition.
+      </h3>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Hitch
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton>
             <Button>
-                Enter Hitch
+                Get Hitch Free
                 <ArrowRight className="h-4 w-4 ml-2"/>
             </Button>
-        </div>
-    )
-}
+        </SignInButton>
+      )}
+    </div>
+  );
+};
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://outgoing-gator-72.clerk.accounts.dev",
+            applicationID: "convex",
+        }
+    ]
+}
