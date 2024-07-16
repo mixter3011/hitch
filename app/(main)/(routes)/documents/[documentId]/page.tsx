0f734ffa198efdd22a38d44cc6c2d31d6d95a718
fobic <sabychakraborty08@gmail.com>
2024-07-17 02:32:26 +0530

    feat(toolbar) : added toolbar to add icons to document.

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,9 +1,44 @@
-const DocumentIdPage  = () => {
+"use client";
+
+import { useQuery } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { Toolbar } from "@/components/toolbar";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return (
+        <div>Loading...</div>
+    );
+  }
+
+  if (document === null) {
     return (
         <div>
-            DocumentId
+            Not found
         </div>
-    );
-}
+    )
+  }
+
+  return (
+    <div className="pb-40">
+        <div className="h-[35vh]"/>
+        <div className="md:mx-w-3xl lg:max-w-4xl mx-auto">
+            <Toolbar initialData={document}/>
+        </div>
+    </div>
+  );
+};
 
 export default DocumentIdPage;
