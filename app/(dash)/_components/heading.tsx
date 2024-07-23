10f445a65d0ceb3c83c7030599ca3ee13dc16f6f
fobic <sabychakraborty08@gmail.com>
2024-07-24 02:53:25 +0530

    optim

--- app/(dash)/_components/heading.tsx
+++ app/(dash)/_components/heading.tsx
@@ -13,7 +13,7 @@ export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        An Intuitive Alternative to Notion. Experience more with{" "}
+        An Alternative to Notion. Experience more with{" "}
         <span className="underline">Hitch</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
